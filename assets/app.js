c7338ecff07970750b8b74689a86836d3bb776d8
André Peuker <andre.peuker@edv-peuker.de>
2024-05-04 00:49:11 +0200

    setup webpack encore with libs

--- assets/app.js
+++ assets/app.js
@@ -5,6 +5,14 @@ import './bootstrap.js';
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
-import './styles/app.css';
+//import './styles/app.css';
+
+import './styles/lib/bootstrap-5.3.3-dist/css/bootstrap.min.css';
+import './styles/lib/fontawesome-pro-6.5.2-web/css/all.min.css'
+import './styles/app.less';
+
+import './js/lib/jquery-3.7.1.min';
+import './styles/lib/bootstrap-5.3.3-dist/js/bootstrap.min';
+import './styles/lib/fontawesome-pro-6.5.2-web/js/all.min.js';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
